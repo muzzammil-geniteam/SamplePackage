3b9f564993f107f26a31862f54995265fca81bcf
muzzammil-geniteam <muzzammilseikh1@gmail.com>
2022-06-27 16:59:58 +0500

    deleted tests

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "SamplePackage",
             dependencies: []),
-        .testTarget(
-            name: "SamplePackageTests",
-            dependencies: ["SamplePackage"]),
     ]
 )
